46e6a654c6e49147046315d6b95844db7ab3e940
Grav3Lord <wolwer23@gmail.com>
2025-06-30 15:54:31 +0300

    Практикуемся в использовании API клиентов. Практика использования API-клиентов.

--- clients/exercises/exercises_client.py
+++ clients/exercises/exercises_client.py
@@ -1,13 +1,52 @@
-from clients.api_client import ApiClient
 from httpx import Response
 
 from typing import TypedDict
+
+from clients.api_client import ApiClient
+from clients.private_http_builder import get_private_http_client, AuthenticationUserDict
+
+
+class Exercise(TypedDict):
+    """
+    Описание структуры задания
+    """
+    id: str
+    title: str
+    courseId: str
+    maxScore: int
+    minScore: int
+    orderIndex: int
+    description: str
+    estimatedTime: str
+
+
+class GetExercisesResponseDict(TypedDict):
+    """
+    Описание структуры ответа получения заданий
+    """
+    exercises: list[Exercise]
+
+
+class GetExerciseResponseDict(TypedDict):
+    """
+    Описание структуры ответа для получения задания.
+    """
+    exercise: Exercise
+
 
 class GetExercisesQueryDict(TypedDict):
     """
     Описание структуры запроса на получение списка курсов.
     """
     courseId: str
+
+
+class CreateExerciseResponseDict(TypedDict):
+    """
+    Описание структуры ответа для создания задания.
+    """
+    exercise: Exercise
+
 
 class CreateExerciseRequestDict(TypedDict):
     """
@@ -20,6 +59,14 @@ class CreateExerciseRequestDict(TypedDict):
     orderIndex: int
     description: str
     estimatedTime: str
+
+
+class UpdateExerciseResponseDict(TypedDict):
+    """
+    Описание структуры ответа для обновления задания.
+    """
+    exercise: Exercise
+
 
 class UpdateExerciseRequestDict(TypedDict):
     """
@@ -45,6 +92,10 @@ class ExercisesClient(ApiClient):
         """
         return self.get("/api/v1/exercises", params=query)
 
+    def get_exercises(self, query: GetExercisesQueryDict) -> GetExercisesResponseDict:
+        response = self.get_exercises_api(query)
+        return response.json()
+
     def get_exercise_api(self, exercise_id: str) -> Response:
         """
         Метод для получения упражнения по его id.
@@ -53,6 +104,10 @@ class ExercisesClient(ApiClient):
         """
         return self.get(f"/api/v1/exercises/{exercise_id}")
 
+    def get_exercise(self, exercise_id: str) -> GetExerciseResponseDict:
+        response = self.get_exercise_api(exercise_id)
+        return response.json()
+
     def create_exercise_api(self, request: CreateExerciseRequestDict) -> Response:
         """
         Метод для создания упражнения.
@@ -60,6 +115,10 @@ class ExercisesClient(ApiClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.post("/api/v1/exercises", json=request)
+
+    def create_exercise(self, request: CreateExerciseRequestDict) -> CreateExerciseResponseDict:
+        response = self.create_exercise_api(request)
+        return response.json()
 
     def update_exercise_api(self, exercise_id: str, request: UpdateExerciseRequestDict) -> Response:
         """
@@ -70,6 +129,10 @@ class ExercisesClient(ApiClient):
         """
         return self.patch(f"/api/v1/exercises/{exercise_id}", json=request)
 
+    def update_exercise(self, exercise_id: str, request: UpdateExerciseRequestDict) -> UpdateExerciseResponseDict:
+        response = self.update_exercise_api(exercise_id, request)
+        return response.json()
+
     def delete_exercise_api(self, exercise_id: str) -> Response:
         """
         Метод для удаления упражнения
@@ -77,3 +140,13 @@ class ExercisesClient(ApiClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.delete(f"/api/v1/exercises/{exercise_id}")
+
+
+def get_exercises_client(user: AuthenticationUserDict) -> ExercisesClient:
+    """
+    Функция создания экземпляра ExercisesClient c уже настроенным HTTPX-клиентом.
+    :param user: Словарь, содержащий username и password.
+    :return: Готовый к использованию ExercisesClient.
+    """
+    client = get_private_http_client(user)
+    return ExercisesClient(client)
